053bcf8ccfaee5377f19dd1815e0dbc3a3734ac0
Vinayak Upadhyay <vinayak7317783517@gmail.com>
2024-08-01 23:26:21 +0530

    Validation and field required done ..

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   const [clickFlag, setClickFlag] = useState(false);
 
   useEffect(() => {
@@ -16,7 +25,7 @@ const Login = () => {
     <div>
       <dialog id="my_modal_3" className="modal">
         <div className="modal-box dark:bg-slate-600 dark:text-white">
-          <form method="dialog">
+          <form onSubmit={handleSubmit(onSubmit)} method="dialog">
             {/* if there is a button in form, it will close the modal */}
             <Link
               to="/"
@@ -25,44 +34,54 @@ const Login = () => {
             >
               ✕
             </Link>
+            <h3 className="font-bold text-2xl text-pink-500">Login</h3>
+            {/*Email */}
+            <div className="mt-5 space-y-2 dark:bg-slate-600 dark:text-black">
+              <span>Email</span>
+              <br />
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="w-80 px-3 py-1 border rounded-md outline-none"
+                {...register("email", { required: true })}
+              />
+              <br />
+              {errors.email && (
+                <span className="text-red-400">This field is required</span>
+              )}
+            </div>
+            {/*Password */}
+            <div className="mt-5 space-y-2 dark:bg-slate-600 dark:text-black">
+              <span>Password</span>
+              <br />
+              <input
+                type="Password"
+                placeholder="Password"
+                className="w-80 px-3 py-1 border rounded-md outline-none"
+                {...register("password", { required: true })}
+              />
+              <br />
+              {errors.password && (
+                <span className="text-red-400">This field is required</span>
+              )}
+            </div>
+            {/*Button*/}
+            <div className="flex justify-around mt-4">
+              <button className="bg-pink-500 text-white px-3 py-1  rounded-md hover:bg-pink-700 duration-200">
+                Login
+              </button>
+              <p>
+                Not registered?{" "}
+                <Link
+                  to="/signup"
+                  className="underline text-blue-500 cursor-pointer"
+                  onClick={() => setClickFlag(!this.clickFlag)}
+                >
+                  Signup
+                </Link>
+              </p>
+            </div>
           </form>
-          <h3 className="font-bold text-2xl text-pink-500">Login</h3>
-          {/*Email */}
-          <div className="mt-5 space-y-2 dark:bg-slate-600 dark:text-black">
-            <span>Email</span>
-            <br />
-            <input
-              type="email"
-              placeholder="Enter your email"
-              className="w-80 px-3 py-1 border rounded-md outline-none"
-            />
-          </div>
-          {/*Password */}
-          <div className="mt-5 space-y-2 dark:bg-slate-600 dark:text-black">
-            <span>Password</span>
-            <br />
-            <input
-              type="Password"
-              placeholder="Password"
-              className="w-80 px-3 py-1 border rounded-md outline-none"
-            />
-          </div>
-          {/*Button*/}
-          <div className="flex justify-around mt-4">
-            <button className="bg-pink-500 text-white px-3 py-1  rounded-md hover:bg-pink-700 duration-200">
-              Login
-            </button>
-            <p>
-              Not registered?{" "}
-              <Link
-                to="/signup"
-                className="underline text-blue-500 cursor-pointer"
-                onClick={() => setClickFlag(!this.clickFlag)}
-              >
-                Signup
-              </Link>
-            </p>
-          </div>
         </div>
       </dialog>
     </div>
--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -2,68 +2,92 @@ import { React, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { useForm } from "react-hook-form";
 
 const CreateUser = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <>
       <Navbar />
       <div className="flex h-screen items-center justify-center ">
         <div className="modal-box w-[600px] dark:bg-slate-600 dark:text-white">
-          <form method="dialog">
+          <form onSubmit={handleSubmit(onSubmit)} method="dialog">
             <Link
               to="/"
               className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
             >
               ✕
             </Link>
+            <h3 className="font-bold text-2xl text-pink-500">Signup</h3>
+            <div className="mt-5 space-y-2 dark:bg-slate-600 dark:text-black">
+              <span>Name</span>
+              <br />
+              <input
+                type="text"
+                placeholder="Enter your full name"
+                className="w-80 px-3 py-1 border rounded-md outline-none"
+                {...register("name", { required: true })}
+              />
+              <br />
+              {errors.name && (
+                <span className="text-red-400">This field is required</span>
+              )}
+            </div>
+            {/*Email */}
+            <div className="mt-5 space-y-2 dark:bg-slate-600 dark:text-black">
+              <span>Email</span>
+              <br />
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="w-80 px-3 py-1 border rounded-md outline-none"
+                {...register("email", { required: true })}
+              />
+              <br />
+              {errors.email && (
+                <span className="text-red-400">This field is required</span>
+              )}
+            </div>
+            {/*Password */}
+            <div className="mt-5 space-y-2 dark:bg-slate-600 dark:text-black">
+              <span>Password</span>
+              <br />
+              <input
+                type="Password"
+                placeholder="Password"
+                className="w-80 px-3 py-1 border rounded-md outline-none"
+                {...register("password", { required: true })}
+              />
+              <br />
+              {errors.password && (
+                <span className="text-red-400">This field is required</span>
+              )}
+            </div>
+            {/*Button*/}
+            <div className="flex justify-around mt-4">
+              <button className="bg-pink-500 text-white px-3 py-1  rounded-md hover:bg-pink-700 duration-200">
+                Signup
+              </button>
+              <p>
+                Have Account?{" "}
+                <Link
+                  onClick={() =>
+                    document.getElementById("my_modal_3").showModal()
+                  }
+                  className="underline text-blue-500 cursor-pointer"
+                >
+                  Login
+                </Link>
+              </p>
+            </div>
           </form>
-          <h3 className="font-bold text-2xl text-pink-500">Signup</h3>
-          <div className="mt-5 space-y-2 dark:bg-slate-600 dark:text-black">
-            <span>Name</span>
-            <br />
-            <input
-              type="text"
-              placeholder="Enter your full name"
-              className="w-80 px-3 py-1 border rounded-md outline-none"
-            />
-          </div>
-          {/*Email */}
-          <div className="mt-5 space-y-2 dark:bg-slate-600 dark:text-black">
-            <span>Email</span>
-            <br />
-            <input
-              type="email"
-              placeholder="Enter your email"
-              className="w-80 px-3 py-1 border rounded-md outline-none"
-            />
-          </div>
-          {/*Password */}
-          <div className="mt-5 space-y-2 dark:bg-slate-600 dark:text-black">
-            <span>Password</span>
-            <br />
-            <input
-              type="Password"
-              placeholder="Password"
-              className="w-80 px-3 py-1 border rounded-md outline-none"
-            />
-          </div>
-          {/*Button*/}
-          <div className="flex justify-around mt-4">
-            <button className="bg-pink-500 text-white px-3 py-1  rounded-md hover:bg-pink-700 duration-200">
-              Signup
-            </button>
-            <p>
-              Have Account?{" "}
-              <Link
-                onClick={() =>
-                  document.getElementById("my_modal_3").showModal()
-                }
-                className="underline text-blue-500 cursor-pointer"
-              >
-                Login
-              </Link>
-            </p>
-          </div>
         </div>
       </div>
 
