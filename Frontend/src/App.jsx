c36b1f6ac4752d792e75516cc4a92f7d3697af94
Vinayak Upadhyay <vinayak7317783517@gmail.com>
2024-08-03 13:59:47 +0530

    Contact page done...

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./home/Home";
 import { Route, Routes } from "react-router-dom";
 import Courses from "./courses/Courses";
 import CreateUser from "./components/Signup";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/course" element={<Courses />} />
           <Route path="/signup" element={<CreateUser />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
     </>
---
+++ Frontend/src/components/Contact.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { useForm } from "react-hook-form";
+const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  //const onSubmit = (data) =>
+  //localStorage.setItem("UserData", JSON.stringify(data));
+
+  return (
+    <>
+      <Navbar />
+      <div className="flex h-screen items-center justify-center ">
+        <div className="">
+          <h1>Contact</h1>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="mt-5 space-y-2 dark:bg-slate-900 dark:text-blue-600">
+              <span>Name</span>
+              <br />
+              <input
+                type="text"
+                placeholder="Enter your name"
+                className="w-80 px-3 py-1 border rounded-md outline-none"
+                {...register("name", { required: true })}
+              />
+              <br />
+              {errors.name && (
+                <span className="text-red-400">This field is required</span>
+              )}
+            </div>
+            <div className="mt-5 space-y-2 dark:bg-slate-900 dark:text-blue-600">
+              <span>Email</span>
+              <br />
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="w-80 px-3 py-1 border rounded-md outline-none"
+                {...register("email", { required: true })}
+              />
+              <br />
+              {errors.email && (
+                <span className="text-red-400">This field is required</span>
+              )}
+            </div>
+            <div className="mt-5 space-y-2 dark:bg-slate-900 dark:text-blue-600">
+              <span>Message</span>
+              <br />
+              <input
+                type="text"
+                placeholder="Enter your message"
+                className="w-80 pb-20 border rounded-md outline-none"
+                {...register("meassage", { required: true })}
+              />
+              <br />
+              {errors.meassage && (
+                <span className="text-red-400">This field is required</span>
+              )}
+            </div>
+            <div className="flex justify-left mt-4">
+              <button className="bg-blue-600 text-white px-3 py-1  rounded-md hover:bg-blue-800 duration-200">
+                Contact
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Contact;
